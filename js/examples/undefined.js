b9c214a4468ed30e5c2349520441f123b962d6b8
Walle Cyril <capocyril@hotmail.com>
2016-06-30 18:56:53 +0200

    empty slots also evaluates to undefined

--- js/examples/undefined.js
+++ js/examples/undefined.js
@@ -9,37 +9,44 @@ const toTest = []; // at the end we test everything inside
 
 
 //0 use undefined
-toTest.push(isUndefinedSafe(undefined));
+toTest.push(undefined);
 
 //1 use void 0
-toTest.push(isUndefinedSafe(void 0));
+toTest.push(void 0);
 
 
 //2 use an unassigned variable
 let a;
-toTest.push(isUndefinedSafe(a));
+toTest.push(a);
 
 //3 works with object members too, also with Maps
 const myObject = {};
-toTest.push(isUndefinedSafe(myObject.abcde));
+toTest.push(myObject.abcde);
 
 //4 use a function with an implicit return undefined
 const returnUndefined = function () {
     return;
 };
-toTest.push(isUndefinedSafe(returnUndefined()));
+toTest.push(returnUndefined());
 
 //4 use an empty function with an implicit return
 const returnUndefined2 = function () {};
-toTest.push(isUndefinedSafe(returnUndefined2()));
+toTest.push(returnUndefined2());
 
 //5 use an empty function with an implicit return just once
 // there also other ways to write this (for example with the function constructor
-toTest.push(isUndefinedSafe((function () {}())));
+toTest.push((function () {}()));
 
-//6 are there other canonical ways ?
+//6 use an empty slot that evaluates to undefined
+const arrayWithEmptySlots = new Array(5);
+toTest.push(arrayWithEmptySlots[0]);
+// or 
+toTest.push((Array(1))[0]); // 1liner
+
+
+//7 are there other ways ?
 
 /*Now tests the results*/
-console.log("all assertions above are " + toTest.every(function (bool) {
-    return bool;
+console.log("all above should be true: " + toTest.every(function (maybeUndefined) {
+    return isUndefinedSafe(maybeUndefined);
 })); // should print true

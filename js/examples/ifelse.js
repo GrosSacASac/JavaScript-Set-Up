9d42f0558f4562bd1e3fdfa41dd6fb75c3034d29
Walle Cyril <capocyril@hotmail.com>
2016-11-16 14:37:12 +0100

    ifelse

---
+++ js/examples/ifelse.js
@@ -0,0 +1,46 @@
+/*if else */
+
+//if else
+
+if (boolean1) {
+    //statements 1
+} else {
+    //statements else
+}
+
+
+//if, else if, else
+
+if (boolean1) {
+    //statements 1
+} else if (boolean2) {
+    //statements 2
+} else if (booleanN) {
+    //statements N
+} else {
+    // statetments else
+}
+
+/*if else using object
+
+*/
+
+const elseIfObject = {
+    "true": function () {
+        //statements 1
+    },
+    "false": function () {
+        //statements else
+    }
+};
+
+elseIfObject[boolean1]();
+
+//todo
+//if, else if, else using object
+
+//...
+
+/*if else using function*/
+
+/*if else using switch*/

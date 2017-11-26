5a28c6c4859aeb5a8d2931c608b8009b129aaf41
Walle Cyril <capocyril@hotmail.com>
2017-11-26 12:58:18 +0100

    refactor gamepad demo

--- js/gamepad/gamepad.js
+++ js/gamepad/gamepad.js
@@ -1,117 +1,144 @@
-/*
+/* demo, inspired by https://github.com/luser/gamepadtest
  */
 
 const gamePadSupport = (
+    /*"Gamepad" in window &&*/
     "GamepadEvent" in window &&
-    window.navigator && window.navigator.getGamepads &&
-    window.requestAnimationFrame
+    window.navigator &&
+    window.navigator.getGamepads
 );
 
-const customRound = function (x, precision) {
-    // can have small errors due to fixed floats precision
-    const rest  = x % precision;
-    if (rest === 0) {
-        return x;
-    } else {
-        const halfPrecision = precision / 2;
-        if (rest > halfPrecision) {
-            return x + (precision - rest);
-        } else {
-            return x - rest;
-        }
-    }
-};
+
+const playerElement = document.getElementById("player");
 
 (function () {
+    if (!gamePadSupport) {
+        playerElement.textContent = "No gamePad support";
+        return;
+    }
+    playerElement.textContent = "Plug in a gamepad and press any button";
+    
+    const SETTINGS = {
+        /* Must be in range 0.0 to 1.0
+        The closer to value to 0, the better it is for precision
+        The closer to value to 1, the better it is for GamePads
+        that are slightly de-calibrated or old */
+        MIN_AXES_VALUE : 0.20,
+        MAX_CONTROLLERS: 8
+    };
+    
+    const lastInput = {
+        anyControllerEverPluggedIn: false,
+        controllers: Array(SETTINGS.MAX_CONTROLLERS)
+    };
+    
     const player = {
         x: 0,
         minX: 0,
-        maxX: innerWidth,
+        maxX: innerWidth - 100 /* leave space for object width */,
         y: 0,
         minY: 0,
-        maxY: innerHeight,
-        element: document.getElementById("player"),
-        speed: 2.5,
-        action: undefined
+        maxY: innerHeight - 100 /* leave space for object height */,
+        speed: 7,
+        action: undefined,
+        element: playerElement
     };
-    if (!gamePadSupport) {
-        player.element.textContent = "No gamePad support";
-        return;
-    }
     
-    var controllers = {};
+    const controllers = {}; // todo make this more like lastInput.controllers
 
-    function connecthandler(event) {
+    const scanGamepadsChrome = function () {
+      // required for chrome
+      // in chrome the gamepad object is only a snapshot (frozen)
+      // to get the last value we have to navigator.getGamepads()
+      // in ff this should have no observable effect
+      const gamepads = navigator.getGamepads();
+      for (let i = 0; i < gamepads.length; i += 1) {
+        if (gamepads[i]) {
+            controllers[gamepads[i].index] = gamepads[i];
+        }
+      }
+      
+    }
+
+    const getLastInput = function () {
+        // updates lastInput, with normalized input
+        scanGamepadsChrome();
+        Object.values(controllers).forEach(function (controller) {
+            const axes = [];  
+            for (let i = 0; i < controller.axes.length; i += 1) {
+                let value = controller.axes[i]; /* -1.0 to +1.0 */
+                if (Math.abs(value) < SETTINGS.MIN_AXES_VALUE) {
+                    value = 0;
+                }
+                console.log(value);
+                axes.push(value);
+            }
+            lastInput.controllers[controller.index] = {
+                buttons: controller.buttons,
+                axes
+            };
+        });
+    };
+    
+    const updateGameState = function () {
+        // reads lastInput and updates the game state
+        const controllerPlayer1 = lastInput.controllers[0]
+        for (let i = 0; i < controllerPlayer1.buttons.length; i += 1) {
+            const {value, pressed} = controllerPlayer1.buttons[i];
+            if (pressed) {
+                if (player.action) {
+                    player.action += ` and ${i}`;
+                } else {
+                    player.action = `Pressed button ${i}`;
+                }
+            }
+        }
+
+        for (let i = 0; i < controllerPlayer1.axes.length; i += 1) {
+            const distance = controllerPlayer1.axes[i] * player.speed;
+            if (i === 0) {
+                player.x += distance;
+                // clamp x
+                player.x = Math.min(Math.max(player.x, player.minX), player.maxX);
+            } else if (i === 1) { 
+                player.y += distance;
+                // clamp y
+                player.y = Math.min(Math.max(player.y, player.minY), player.maxY);
+            }
+        }
+    };
+    
+    const updateDisplay = function () {
+        // reads the game state and updates the display
+        player.element.style.left = `${player.x}px`;
+        player.element.style.top = `${player.y}px`;
+        if (player.action) {
+            player.element.textContent = player.action;
+            player.action = undefined;
+        }
+    };
+    
+    function loop() {
+
+        getLastInput();
+        updateGameState();
+        updateDisplay();
+        requestAnimationFrame(loop);
+    }
+
+    window.addEventListener("gamepadconnected", function connecthandler(event) {
         const gamepad = event.gamepad;
         controllers[gamepad.index] = gamepad;
-    }
-
-    function disconnecthandler(event) {
-      const gamepad = event.gamepad
-      delete controllers[gamepad.index];
-    }
-
-    function loop() {
-      scangamepads();
-      let j;
-      Object.values(controllers).forEach(function (controller) {
-        
-        for (var i=0; i<controller.buttons.length; i++) {
-          
-          const {value, pressed} = controller.buttons[i];
-          if (pressed) {
-            if (player.action) {
-                player.action += ` and ${i}`;
-            } else {
-                player.action = `Pressed button ${i}`;
-            }
-          }
+        if (!lastInput.anyControllerEverPluggedIn) {
+            lastInput.anyControllerEverPluggedIn = true;
+            player.element.textContent = "Move me";
+            requestAnimationFrame(loop);
         }
-        
+    });
+    
+    window.addEventListener("gamepaddisconnected", function (event) {
+        const gamepad = event.gamepad
+        delete controllers[gamepad.index];
+    });
 
-        for (var i=0; i<controller.axes.length; i++) {
-            let value = controller.axes[i];
-            if (Math.abs(value) < 0.15) {
-                //deal with somethwhat decalibrated axes
-                value = 0;
-            }
-            value *= player.speed;
-            if (i === 0) {
-                player.x += value;  
-            } else if (i === 1) { 
-          
-                player.y += value;  
-            }
-          
-        }
-      });
-      player.element.style.left = `${player.x}px`;
-      player.element.style.top = `${player.y}px`;
-      if (player.action) {
-        player.element.textContent = player.action;
-        player.action = undefined;
-      }
-      requestAnimationFrame(loop);
-    }
-
-    function scangamepads() {
-      var gamepads = navigator.getGamepads();
-      for (var i = 0; i < gamepads.length; i++) {
-        if (gamepads[i]) {
-          if (!(gamepads[i].index in controllers)) {
-            addgamepad({
-                gamepad: gamepads[i]
-            });
-          } else {
-            controllers[gamepads[i].index] = gamepads[i];
-          }
-        }
-      }
-    }
-
-
-    window.addEventListener("gamepadconnected", connecthandler);
-    window.addEventListener("gamepaddisconnected", disconnecthandler);
-
-    requestAnimationFrame(loop);
 }());

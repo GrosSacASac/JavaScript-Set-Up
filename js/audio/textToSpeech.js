fda7413da02944699cf2452679d96926b07603d3
Walle Cyril <capocyril@hotmail.com>
2018-05-31 18:06:33 +0200

    Update textToSpeech.js

--- js/audio/textToSpeech.js
+++ js/audio/textToSpeech.js
@@ -1,5 +1,5 @@
 
-const utterance = new SpeechSynthesisUtterance();
+const speechSynthesisUtterance = new SpeechSynthesisUtterance();
 utterance.lang = "en";
 utterance.text = "Hi what is up ?";
 speechSynthesis.speak(speechSynthesisUtterance);

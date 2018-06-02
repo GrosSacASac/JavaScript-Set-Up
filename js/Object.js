51a92b3bd093bebf192f8873caf7168bc8cb86a1
Walle Cyril <capocyril@hotmail.com>
2018-06-02 12:43:26 +0200

    remove, Object.seal Object.preventExtensions Object.freeze

--- js/Object.js
+++ js/Object.js
@@ -12,13 +12,19 @@ anObject = {};
 // without prototype
 anObject = Object.create(null);
 
+// creating with prefilled properties
+anObject = {
+    "key1": "value1",
+    "key2": "value2",
+};
+
 // assign simple
 anObject["key"] = "value";
 
 // assign multiple
 Object.assing(anObject, {
     "key1": "value1",
-    "key2": "value2",
+    "key2": "value2"
 });
 
 // get a specific value
@@ -45,8 +51,31 @@ Object.entries(anObject).forEach(function ([key, value]) {
 
 });
 
-// has
+// has a key
 anObject.hasOwnProperty("key");
 
 // has safe, works even when anObject has a key "hasOwnProperty"
 Object.prototype.hasOwnProperty.call(anObject, "key");
+
+// remove a value
+anObject["key"] = undefined;
+
+// completly remove a property (key and value)
+delete anObject["key"];
+
+// prevent future extensions
+Object.preventExtensions(anObject);
+anObject["newThing"] = 2; // Error in strict mode
+
+// prevent future extensions and removals
+anObject["beforeSealing"] = 10;
+Object.seal(anObject);
+anObject["newThing"] = 2; // Error in strict mode
+delete anObject["beforeSealing"]; // Error in strict mode
+
+// prevent future extensions and removals and mutations
+anObject["beforeSealing"] = 10;
+Object.freeze(anObject);
+anObject["newThing"] = 2; // Error in strict mode
+delete anObject["beforeSealing"]; // Error in strict mode
+anObject["beforeSealing"] = 11; // Error in strict mode

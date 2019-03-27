abb208b73d8cad68c7fdb08b1fc5096af9c7b3ca
Walle Cyril <cyril.walle@protonmail.com>
2019-03-27 22:03:50 +0100

    decide takes actionNames only, learn takes reward directly

--- js/Qai.js
+++ js/Qai.js
@@ -15,7 +15,7 @@ const draw = (state) => {
             } else if (state.rewards[0][0] === x && state.rewards[0][1] === y) {
                 line.push(`@`);
             } else {
-                    line.push(` `)
+                line.push(` `);
             }
         }
         console.log(line.join(` `));
@@ -30,7 +30,7 @@ const initialState = {
     dangers: [[3,3], [2,2],[4,1],[3,2]],
     score: 0,
     dangersTouched: [],
-    rewardsTouched: []
+    rewardsTouched: [],
 };
 
 const hashState = (state) => {
@@ -74,9 +74,6 @@ const actions = {
         }
         state.position[1] = futureY;
     },
-    // standBy: () => {
-    //
-    // }
 };
 
 const updateGame = (action, state) => {
@@ -93,7 +90,7 @@ const updateGame = (action, state) => {
         if (x === X && y === Y) {
             state.score += 1;
             state.position = [0,0];
-            state.rewardsTouched.push(frame)
+            state.rewardsTouched.push(frame);
         }
     });
 };
@@ -102,12 +99,12 @@ const intelligence = {
     learnFactor: 0.5,
     discountFactor: 0.9,
     exploreBonus: 0.04,
-    decide: (hashedState, actions) => {
+    decide: (hashedState, actionNames) => {
         let qualityForState = intelligence.qualityMap[hashedState];
         if (!qualityForState) {
             // first time in this situation
-            intelligence.qualityMap[hashedState] = Object.keys(actions).map(action => {
-                return [0, action]
+            intelligence.qualityMap[hashedState] = actionNames.map(actionName => {
+                return [0, actionName];
             });
             qualityForState = intelligence.qualityMap[hashedState];
             // console.log(qualityForState)
@@ -117,19 +114,18 @@ const intelligence = {
         })[0];
 
         // console.log(119,highestQuality, qualityForState)
-        return actions[highestQualityActionName];
+        return highestQualityActionName;
     },
     qualityMap: {
 
     },
-    learn:(previousHashedState, hashedStateAfter, scoreBefore, scoreAfter) => {
+    learn:(previousHashedState, hashedStateAfter, reward) => {
         // still sorted
         const qualityForState = intelligence.qualityMap[previousHashedState];
         if (previousHashedState === hashedStateAfter) {
             qualityForState[0][0] += -intelligence.exploreBonus;
             return;
         }
-        const immediateReward = scoreAfter - scoreBefore;
         const nextQualityForState = intelligence.qualityMap[hashedStateAfter];
         let nextMaxQualityForState = 0;
         let nextHighestQualityActionName = 'unused';
@@ -140,36 +136,39 @@ const intelligence = {
         }
         // console.log(144, nextQualityForState, nextMaxQualityForState);
         qualityForState[0][0] += intelligence.learnFactor * (
-            immediateReward +
+            reward +
             intelligence.discountFactor * (nextMaxQualityForState - qualityForState[0][0])
         ) -intelligence.exploreBonus;
-        // if (intelligence.exploreBonus > 0.04) {
-        //     intelligence.exploreBonus -= 0.001;
-        // }
-
     },
 };
 
-const state = initialState;
+const report = () => {
+    console.log(intelligence.qualityMap)
+    console.log(`total frames ${frame}`)
+    console.log(`times touched rewards ${state.rewardsTouched.length}` );
+    console.log(`times touched danger ${state.dangersTouched.length} at frames: ${state.dangersTouched}`);
+};
 
+const state = initialState;
+const actionNames = Object.keys(actions)
 const step = () => {
     const hashedState = hashState(state);
     const scoreBefore = state.score;
-    const action = intelligence.decide(hashedState, actions);
+    const actionName = intelligence.decide(hashedState, actionNames);
+    const action = actions[actionName];
     updateGame(action, state); // reward and changes state
     // draw(state);
     const hashedStateAfter = hashState(state);
     const scoreAfter = state.score;
-    intelligence.learn(hashedState, hashedStateAfter, scoreBefore, scoreAfter);
+    const reward = scoreAfter - scoreBefore;
+    intelligence.learn(hashedState, hashedStateAfter, reward);
     frame++;
     if (frame < MAX_FRAMES) {
         setTimeout(step, DELAY)
     } else {
-        console.log(intelligence.qualityMap)
-        console.log(`total frames ${frame}`)
-        console.log(`times touched rewards ${state.rewardsTouched.length}` );
-        console.log(`times touched danger ${state.dangersTouched.length} at frames: ${state.dangersTouched}`);
+        report();
     }
 };
+
 
 step();

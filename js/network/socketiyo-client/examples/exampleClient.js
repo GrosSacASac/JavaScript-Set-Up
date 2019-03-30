ac85b5f68fdcf48dbd7aec7fa87ebedd89b63cb0
Walle Cyril <cyril.walle@protonmail.com>
2019-03-30 16:29:45 +0000

    better example

--- js/network/socketiyo-client/examples/exampleClient.js
+++ js/network/socketiyo-client/examples/exampleClient.js
@@ -10,10 +10,9 @@ const socketiyoConnection = createConnection({url: `ws://localhost:8080/`});
 
 socketiyoConnection.on(DEFAULT_CHANNEL, (x) => {
     console.log(x);
-    document.body.textContent += x;
 });
 
 socketiyoConnection.on(`game/end`, (x) => {
-    console.log(`game/end`);
-    document.body.textContent += `game/end`;
+    console.log(`game/end ${x}`);
+    document.body.textContent += x;
 });

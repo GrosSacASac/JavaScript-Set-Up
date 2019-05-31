3281eb56d0ca804089a357bc934030a89ab340c5
Walle Cyril <cyril.walle@protonmail.com>
2019-05-31 16:56:05 +0200

    disconnection detection draft

---
+++ js/network/socketiyo/source/disconnectionDetection.js
@@ -0,0 +1,41 @@
+export {useAdditionalDisconnectionDetection};
+
+import {
+    RECEIVE_MESSAGE,
+    RECEIVE_SUBSCRIBE,
+    RECEIVE_UNSUBSCRIBE,
+    LAST_CONNECTION_CHECK,
+} from "./socketiyo.js";
+
+
+const markSocketLastConnectionCheck = socket => {
+    socket[LAST_CONNECTION_CHECK] = Date.now()
+};
+
+const markSocketLastConnectionCheckAdapter = ({socket}) => {
+    markSocketLastConnectionCheck(socket);
+};
+
+const useAdditionalDisconnectionDetection = ({
+    socketiyoServer,
+    disconnectionCheckInterval = 20000, /* the smaller, the faster disconnection are detected
+    at a cost of bandwidth and cpu */
+}) => {
+    /* slightly less to check every time, (setInterval is slightly time volatile) */
+    const minimumTimeBetweenChecks = Math.round(0.9 * disconnectionCheckInterval);
+
+    socketiYoServer.on(RECEIVE_MESSAGE, markSocketLastConnectionCheckAdapter);
+    socketiYoServer.on(RECEIVE_SUBSCRIBE, markSocketLastConnectionCheckAdapter);
+    socketiYoServer.on(RECEIVE_UNSUBSCRIBE, markSocketLastConnectionCheckAdapter);
+
+    const intervalId = setInterval(() => {
+        const now = Date.now();
+        socketiYoServer.connectionsPool.forEach(socket => {
+            const timePassed = now - socket[LAST_CONNECTION_CHECK];
+        });
+        if (timePassed < minimumTimeBetweenChecks) {
+            return;
+        }
+        // todo check, ping
+    }, disconnectionCheckInterval);
+};

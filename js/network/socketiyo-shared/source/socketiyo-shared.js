8824fdedaca22c41733e2c79a239583dc4d48392
Walle Cyril <cyril.walle@protonmail.com>
2019-07-04 23:21:50 +0200

    prepare custom data fromat possibility

--- js/network/socketiyo-shared/source/socketiyo-shared.js
+++ js/network/socketiyo-shared/source/socketiyo-shared.js
@@ -1,30 +1,19 @@
 export {
-	packData,
-	unpackData,
-	formatSend,
-	SUBSCRIBE_CHANNEL_ACTION,
-	UNSUBSCRIBE_CHANNEL_ACTION,
-	DEFAULT_CHANNEL
+    defaultPackData,
+    defaultUnpackData,
+    SUBSCRIBE_CHANNEL_ACTION,
+    UNSUBSCRIBE_CHANNEL_ACTION,
+    DEFAULT_CHANNEL
 };
 
 const DEFAULT_CHANNEL = ``;
 const SUBSCRIBE_CHANNEL_ACTION = `SUBSCRIBE_CHANNEL_ACTION`;
 const UNSUBSCRIBE_CHANNEL_ACTION = `UNSUBSCRIBE_CHANNEL_ACTION`;
 
-// encapsulate
 const packData = (data) => {
-	return JSON.stringify(data);
+    return JSON.stringify(data);
 };
 
 const unpackData = (data) => {
-	return JSON.parse(data);
+    return JSON.parse(data);
 };
-
-const formatSend = (data, channel) => {
-	const toSend = { channel, data };
-	return packData(toSend);
-};
-
-
-
-

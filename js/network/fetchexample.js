1e0f89437a40b6bc4c1cd77c11badf7e8bb9116c
Walle Cyril <cyril.walle@protonmail.com>
2019-07-16 15:47:11 +0200

    Update fetchexample.js

--- js/network/fetchexample.js
+++ js/network/fetchexample.js
@@ -1,13 +1,14 @@
 //see https://fetch.spec.whatwg.org/#dom-global-fetch
 
-const headers = new Headers({ "Content-Type": "application/json"});//can have more
-const input = "something.json";//input can be an URL string or Request instance
-const options = {//dont forget , if you remove some comments
-    method: "GET", //list of safe methods https://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html,
+const headers = new Headers({ [`Content-Type`]: `application/json` }); //can have more
+const input = `something.json`; //input can be an URL string or Request instance
+//dont forget , if you remove some comments
+const options = {
+    method: `GET`, //list of safe methods https://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html,
     headers: headers,
-    body: "" /*//Blob or BufferSource or FormData or URLSearchParams or USVString
-    referrer: "",//string
-   referrerPolicy: "", 
+    body: `` /*//Blob or BufferSource or FormData or URLSearchParams or USVString
+    referrer: ``,//string
+   referrerPolicy: ``, 
    //https://w3c.github.io/webappsec-referrer-policy/#enumdef-referrerpolicy
    mode: RequestMode,
    credentials: RequestCredentials,
@@ -20,26 +21,25 @@ const options = {//dont forget , if you remove some comments
 
 //options is optional
 fetch(input, options).then(function (response) {
-/*
-    most properties are read only, can use response.clone() or new Response()
-    response.url // String
-    response.redirected; // boolean
-    response.status; // short Number
-    response.ok; // boolean 
-    response.statusText; // String
-    response.headers; // Headers
-    response.body; // null or ReadableStream;
-    
-    the following return Promises:
-    response.text(); 
-    response.arrayBuffer();
-    response.blob();
-    response.formData();
-    response.json();
-
-*/
+    /*
+        most properties are read only, can use response.clone() or new Response()
+        response.url // String
+        response.redirected; // boolean
+        response.status; // short Number
+        response.ok; // boolean 
+        response.statusText; // String
+        response.headers; // Headers
+        response.body; // null or ReadableStream;
+        
+        the following return Promises:
+        response.text(); 
+        response.arrayBuffer();
+        response.blob();
+        response.formData();
+        response.json();
+    */
     return response.text();
-}).then(function(text) {
-    const anObject = JSON.parse(text);
+}).then(function (text) {
+    
     //...
 });

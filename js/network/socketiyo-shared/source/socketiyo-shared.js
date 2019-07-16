0d117615d89a529c9834b8c57d93f44c2b5db464
Walle Cyril <cyril.walle@protonmail.com>
2019-07-16 22:43:42 +0200

    fix export

--- js/network/socketiyo-shared/source/socketiyo-shared.js
+++ js/network/socketiyo-shared/source/socketiyo-shared.js
@@ -10,10 +10,10 @@ const DEFAULT_CHANNEL = ``;
 const SUBSCRIBE_CHANNEL_ACTION = `SUBSCRIBE_CHANNEL_ACTION`;
 const UNSUBSCRIBE_CHANNEL_ACTION = `UNSUBSCRIBE_CHANNEL_ACTION`;
 
-const packData = (data) => {
+const defaultPackData = (data) => {
     return JSON.stringify(data);
 };
 
-const unpackData = (data) => {
+const defaultUnpackData = (data) => {
     return JSON.parse(data);
 };

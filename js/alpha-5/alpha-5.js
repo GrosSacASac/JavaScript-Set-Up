c2f365dc39e2b469bf8466f531397538e6217944
Walle Cyril <cyril.walle@protonmail.com>
2020-07-09 12:11:25 +0200

    temp

--- js/alpha-5/alpha-5.js
+++ js/alpha-5/alpha-5.js
@@ -52,37 +52,49 @@ const addNumberIntoByte = (number, existingByte = 0, offset, BitLength = BIT_LEN
 const _compactUInt8ArrayFrom0To31Numbers = _0To31Numbers => {
     const byteLength = Math.ceil((_0To31Numbers.length * BIT_LENGTH) / BYTE);
     const compactUInt8Array = new Uint8Array(byteLength);
-    const enoughSpace = BYTE - BIT_LENGTH;
 
-    let offset = 0;
-    let remainder = 0;
+    let bitPosition = 0;
+    let currentByte = 0;
     let byteIndex = 0;
     let lastPrintNeeded = false;
+    const saveByte = (number) => {
+        console.log(`save ${number} (${number.toString(2)})`)
+        compactUInt8Array[byteIndex] = number;
+        byteIndex += 1;
+        currentByte = 0;
+        bitPosition = 0;
+    };
     _0To31Numbers.forEach(number => {
-        if (offset <= enoughSpace) {
-            remainder = number << (enoughSpace - offset);
-            offset += BIT_LENGTH;
-            lastPrintNeeded = true;
-        } else {
-            const toShift = BYTE - BIT_LENGTH - offset;
-            const nextOffset = BIT_LENGTH - toShift;
-            const toAdd = number << toShift;
-            remainder += toAdd;
-
-            compactUInt8Array[byteIndex] = remainder;
-            byteIndex += 1;
-
-            remainder = (number - toAdd) << nextOffset;
-            offset = nextOffset;
-            lastPrintNeeded = false;
+        if (bitPosition + BIT_LENGTH < BYTE) { // can fit at once
+            const toShift = BYTE - BIT_LENGTH - bitPosition;
+            currentByte += number << toShift;
+            bitPosition += BIT_LENGTH;
+            
+            if (bitPosition === BYTE) {
+                saveByte(currentByte);
+            }
+        } else { // cannot fit at once
+            const shiftRight = -BYTE + bitPosition + BIT_LENGTH; // 2
+            const missedBits = BIT_LENGTH - shiftRight; // 3
+            const clamped = number >> shiftRight;
+            currentByte += clamped;
+            
+            saveByte(currentByte);
+            
+            const substractHead = clamped << shiftRight;
+            const rest = number - substractHead;
+            currentByte += rest << (BYTE+1) - missedBits;
+            bitPosition += missedBits;
         }
     });
     // todo handle all cases
-    if (lastPrintNeeded) {
-        compactUInt8Array[byteIndex] = remainder;
-    } else {
-        compactUInt8Array[byteIndex] = remainder;
-    }
+    console.log(90,currentByte)
+    if (currentByte) {
+        saveByte(currentByte);
+    } //else {
+        //     compactUInt8Array[byteIndex] = remainder;
+        // }
+        console.log(96,compactUInt8Array)
     return compactUInt8Array;
 };
 
@@ -149,24 +161,29 @@ const decode = uInt8Array => {
     return string;
 };
 
-console.log(encode('abc def zxw aaa zzz'));
-console.log(encode('a - z'));
-console.log(encode('a'));
-console.log(encode('ab'));
-console.log(encode('z'));
-console.log(encode('aa'));
-console.log(encode('zz'));
-console.log(encode('\n\n'));
+console.log(37,_compactUInt8ArrayFrom0To31Numbers([1,2,3]))
+console.log(38,Uint8Array.from([
+    0b00001000, // 1-2
+    0b10000110, // 2-3
+]))
+// console.log(encode('abc def zxw aaa zzz'));
+// console.log(encode('a - z'));
+// console.log(encode('a'));
+// console.log(encode('ab'));
+// console.log(encode('z'));
+// console.log(encode('aa'));
+// console.log(encode('zz'));
+// console.log(encode('\n\n'));
 
-console.log(decode(encode('a')));
-console.log(decode(encode('z')));
-console.log(decode(encode('?')));
+// console.log(decode(encode('a')));
+// console.log(decode(encode('z')));
+// console.log(decode(encode('?')));
 
-console.log(decode(encode('aa')));
-console.log(decode(encode('a - z')));
-console.log()
-console.log()
-console.log(decode(encode('abc def zxw aaa zzz')));
-console.log()
-console.log()
-console.log(decode(encode('ab   ')));
+// console.log(decode(encode('aa')));
+// console.log(decode(encode('a - z')));
+// console.log()
+// console.log()
+// console.log(decode(encode('abc def zxw aaa zzz')));
+// console.log()
+// console.log()
+// console.log(decode(encode('ab   ')));
--- js/alpha-5/tests/specification/alpha-5.js
+++ js/alpha-5/tests/specification/alpha-5.js
@@ -5,6 +5,7 @@ import {
     _0To31NumbersFromString,
     _compactUInt8ArrayFrom0To31Numbers,
 } from "../../alpha-5.js";
+import { deepEqualAdded } from "../../deep.js";
 
 
 test(`_0To31NumberFromCharacter a`, t => {
@@ -24,17 +25,30 @@ test(`_0To31NumberFromCharacter az`, t => {
 test(`_0To31NumberFromCharacter discards character out of range`, t => {
     t.deepEqual(_0To31NumbersFromString('aàz'), [0,25]);
 });
-
+/*
 test(`_compactUInt8ArrayFrom0To31Numbers 0`, t => {
     t.deepEqual(_compactUInt8ArrayFrom0To31Numbers([0]), new Uint8Array([0b0]));
 });
 test(`_compactUInt8ArrayFrom0To31Numbers 25`, t => {
     t.deepEqual(_compactUInt8ArrayFrom0To31Numbers([25]), new Uint8Array([0b11001000]));
 });
-
-test(`_compactUInt8ArrayFrom0To31Numbers 25, 25`, t => {
-    t.deepEqual(_compactUInt8ArrayFrom0To31Numbers([25, 25]), new Uint8Array([0b11001110,0b01000000]));
+test(`_compactUInt8ArrayFrom0To31Numbers 25, 25`, t => {    
+    t.deepEqual(_compactUInt8ArrayFrom0To31Numbers([25, 25]), new Uint8Array([0b11001110,0b0100000]));
 });
+*/
+test(`_compactUInt8ArrayFrom0To31Numbers 
+1, 00001
+2, 00010
+3, 00011`, t => {
+
+    t.deepEqual(_compactUInt8ArrayFrom0To31Numbers([1,2,3
+    ]), Uint8Array.from([
+        0b00001000, // 1-2
+        0b10000110, // 2-3
+    ]));
+
+});
+/*
 test(`_compactUInt8ArrayFrom0To31Numbers 
     1, 00001
     2, 00010
@@ -52,4 +66,16 @@ test(`_compactUInt8ArrayFrom0To31Numbers
         0b10011000, // 5-6-7
         0b11101000, // 7-8
     ]));
+    
+    console.log(37,_compactUInt8ArrayFrom0To31Numbers([1,2,3,4,5,6,7,8
+    ]))
+    console.log(38,Uint8Array.from([
+        0b00001000, // 1-2
+        0b10000110, // 2-3-4
+        0b01000010, // 4-5
+        0b10011000, // 5-6-7
+        0b11101000, // 7-8
+    ]))
 });
+/*
+*/
